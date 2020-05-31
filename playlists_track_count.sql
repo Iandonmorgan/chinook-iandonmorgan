34594ad687487962c41631964bf724b3e2a10059
Landon Morgan <morganlandon@gmail.com>
2020-05-31 13:17:01 -0500

    removed comment question, as I figured out tabs on TablePlus are a thing...

--- playlists_track_count.sql
+++ playlists_track_count.sql
@@ -3,5 +3,3 @@ FROM PlaylistTrack pt
 JOIN Playlist p
 ON p.PlaylistId = pt.PlaylistId
 GROUP BY pt.PlaylistId
-
--- curious why the data for PlaylistTrack in TablePlus doesn't look like it has the full data, looks like it only has data for PlaylistId 1.
