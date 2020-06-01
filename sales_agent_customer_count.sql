8103105d83114c527e61b4d877b582a8335fc15b
Landon Morgan <morganlandon@gmail.com>
2020-06-01 15:49:45 -0500

    accidentally used SUM where I should've used COUNT

--- sales_agent_customer_count.sql
+++ sales_agent_customer_count.sql
@@ -1,4 +1,4 @@
-SELECT (e.FirstName || ' ' || e.LastName) as 'Agent Name', SUM(c.SupportRepId) as 'Total Customers'
+SELECT (e.FirstName || ' ' || e.LastName) as 'Agent Name', COUNT(c.SupportRepId) as 'Total Customers'
 FROM Employee e
 JOIN Customer c
 ON c.SupportRepId = e.EmployeeId
